69ab525e248b05e43a43c3b5b18d1c1ded99f798
Krista Kaza <krikaz50070@stud.noroff.no>
2024-10-09 11:29:45 +0300

    changing styles in product page

--- online-shop/src/components/Footer.js
+++ online-shop/src/components/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2024 Online Shop</p>
+      <p>E-Com &copy; 2024 Online Shop</p>
     </footer>
   );
 };
